325ac6fa2e0f560d5a5406ebf6874e7556e0727b
satyaejjurotu <satyaejjurotu@yahoo.com>
2022-04-10 14:26:48 +0400

    first commit

---
+++ smsapp_demo/src/main/java/com/greatlearning/smsapp_demo/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.greatlearning.smsapp_demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.greatlearning.smsapp_demo.entity.Student;
+
+
+public interface StudentService {
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student thestudent);
+
+	public void deleteById(int theId);
+
+}
